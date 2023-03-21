4f44eff0f043f9c8477f67138aafa928d0bf350a
Grzegorz Orzechowski <2590699+gorzech@users.noreply.github.com>
2023-03-21 09:16:15 +0200

    Small fix plus csv from the base call.

--- mcts_single_D_base.jl
+++ mcts_single_D_base.jl
@@ -6,4 +6,4 @@ opts = PendulumOpts()
 envfun = () -> InvertedPendulumEnv(opts)
 
 b = single_pendulum_batch(seed_shift = 1:10, planner_file_name = nothing)
-execute_batch(b[1:10], file_name, envfun = envfun, show_progress = true, start_new_file = true)
+execute_batch(b, file_name, envfun = envfun, show_progress = true, start_new_file = true)
