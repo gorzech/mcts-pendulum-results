aaf34c8f380c4ee94053e9287671a1b42fe206f8
Grzegorz Orzechowski <2590699+gorzech@users.noreply.github.com>
2023-03-30 13:42:42 +0300

    Add csv files for the Ah AI and AJ

---
+++ mcts_single_AJ_exp_x_a_penalty_6_args.jl
@@ -0,0 +1,12 @@
+include("mcts_single.jl")
+
+file_name = "sp_AJ_20230328"
+Environments.reward(env::InvertedPendulumEnv) = reward_cart_angle_use_exp(
+    env,
+    cart_share = 0.25,
+    angle_share = 0.75,
+    cart_dispersion_factor = 0.4,
+    angle_dispersion_factor = 0.25, # same as AA
+)
+
+execute_batch_single_args(ARGS; file_name = file_name, max_force = 10.0)
