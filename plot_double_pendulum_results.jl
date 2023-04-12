589b2bc33d68dfd006620ca248092ca62fa79898
Grzegorz Orzechowski <2590699+gorzech@users.noreply.github.com>
2023-04-12 23:51:49 +0300

    Add newest K case plots

--- plot_double_pendulum_results.jl
+++ plot_double_pendulum_results.jl
@@ -2,8 +2,8 @@
 include("plot_helper.jl")
 
 ## Double pendulun basic reward
-file_name = "csv/dp_J_20230411.csv"
-fig_dir = "fig/dp_J"
+file_name = "csv/dp_K_20230411.csv"
+fig_dir = "fig/dp_K"
 max_mean = 200
 
 ## Read all files
