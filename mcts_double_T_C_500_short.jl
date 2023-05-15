71bde16ef536af205fe207d9298904d204e491c2
Grzegorz Orzechowski <2590699+gorzech@users.noreply.github.com>
2023-05-15 19:59:58 +0300

    Update cases for T

--- mcts_double_T_C_500_short.jl
+++ mcts_double_T_C_500_short.jl
@@ -2,6 +2,6 @@ include("mcts_double.jl")
 
 file_name = "dp_T_20230501" 
 
-dp_batch = DpBatch(γ = 0.9:0.05:1.0, exploration_param = [64, 128, 256])
+dp_batch = DpBatch(γ = 0.9:0.05:1.0, exploration_param = [0, 2, 4, 8])
 
 execute_batch_double_args(ARGS; file_name=file_name, dp_batch=dp_batch, max_steps=500)
